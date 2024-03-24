a0b9303e6d719f03347534a6018ede86f6eac3c1
nwrenger <nils@wrenger.net>
2024-03-24 18:46:55 +0100

    :sparkles: Version dump && Minor ui changes

--- view/src/routes/Popup.svelte
+++ view/src/routes/Popup.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { audio_interface } from '$lib/stores';
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="space-y-2">
+<div class="space-y-4">
 	<div>
 		<div class="flex">
 			<span class="font-bold flex-auto">shitboard</span>
@@ -21,34 +20,14 @@
 			>
 		</div>
 	</div>
-	<Accordion>
-		<AccordionItem>
-			<svelte:fragment slot="lead"><i class="fa-solid fa-circle-info"></i></svelte:fragment>
-			<svelte:fragment slot="summary">Info</svelte:fragment>
-			<svelte:fragment slot="content">
-				<p>
-					A Chaos-Fueled Soundboard App where creativity and permanence collide. Unleash your sounds
-					into the wild with no take-backs. Upload anything; once it's up, it's there forever!
-				</p>
-			</svelte:fragment>
-		</AccordionItem>
-		<!-- svelte-ignore a11y-label-has-associated-control -->
-		<AccordionItem>
-			<svelte:fragment slot="lead"><i class="fa-solid fa-gear"></i></svelte:fragment>
-			<svelte:fragment slot="summary">Settings</svelte:fragment>
-			<svelte:fragment slot="content">
-				<label class="label">
-					<span>Volume</span>
-					<input
-						type="range"
-						max="1.0"
-						min="0.0"
-						step="0.01"
-						bind:value={$audio_interface.volume}
-					/>
-				</label>
-			</svelte:fragment>
-		</AccordionItem>
-	</Accordion>
+
+	<p>
+		A Chaos-Fueled Soundboard App where creativity and permanence collide. Unleash your sounds into
+		the wild with no take-backs. Upload anything; once it's up, it's there forever!
+	</p>
+	<label class="label">
+		<span>Volume</span>
+		<input type="range" max="1.0" min="0.0" step="0.01" bind:value={$audio_interface.volume} />
+	</label>
 </div>
 <div class="arrow bg-surface-100-800-token" />
