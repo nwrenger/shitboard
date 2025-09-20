ccf7e41494787cb351c9e03f62ca644c948cc1ae
Nils Wrenger <nils@wrenger.net>
2025-09-20 15:48:26 +0200

    :bug: Fixed link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -100,7 +100,7 @@
 		</p>
 
 		<div class="flex flex-wrap items-center gap-4">
-			<a href="/privacy-policy" class="underline text-center">Privacy Policy</a>
+			<a href="https://nwrenger.dev/privacy-policy" class="underline text-center">Privacy Policy</a>
 		</div>
 	</div>
 </footer>
